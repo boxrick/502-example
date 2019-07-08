ebf75d724c85dc8be550b2a3bfb926de2d0a6b46
Rick Box <boxrick@gmail.com>
2019-07-08 14:54:51 +0100

    Initial commit

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// echo date('h:i:s') . "\n";
+// sleep(2);
+// echo date('h:i:s') . "\n";
+
+$headers = getallheaders();
+foreach($headers as $key=>$val){
+  echo $key . ': ' . $val . '<br>';
+}
+echo gethostname();
+?>
+<br/>

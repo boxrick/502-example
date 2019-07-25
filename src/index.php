b092dd7c6a799f0b7b62838e5d195778d4f8f42f
Rick Box <boxrick@gmail.com>
2019-07-25 16:28:04 +0100

    Change php script

--- src/index.php
+++ src/index.php
@@ -4,10 +4,5 @@ echo date('h:i:s') . "\n";
 sleep(2);
 echo date('h:i:s') . "\n";
 
-$headers = getallheaders();
-foreach($headers as $key=>$val){
-  echo $key . ': ' . $val . '<br>';
-}
-echo gethostname();
 ?>
 <br/>
